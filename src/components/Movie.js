8419a5d3917a04a97b4fbd5469bbd97e424a704d
Sunoo <wldbsl312@gmail.com>
2022-07-16 22:01:06 +0900

    수정

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,13 +5,13 @@ import styles from "./Movie.module.css";
 function Movie({ id, coverImg, title, year, summary, genres }) {
   return (
     <div className={styles.movie}>
-      <img src={coverImg} alt={title} />
+      <img src={coverImg} alt={title} className={styles.movie__img} />
       <div>
         <h2 className={styles.movie__title}>
           <Link to={`/movie/${id}`}>{title}</Link>
         </h2>
         <h3 className={styles.movie__year}>{year}</h3>
-        <p>{summary}</p>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
         <ul className={styles.movie__genres}>
           {genres.map((g) => (
             <li key={g}>{g}</li>
